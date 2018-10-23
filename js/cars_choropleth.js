d6a6fcb794b0b5df4fb325d44722807dbf8809d8
deepakarnani <ddkarnani@yahoo.com>
2018-10-23 00:26:40 -0400

    adding updated choropleth

--- js/cars_choropleth.js
+++ js/cars_choropleth.js
@@ -12,9 +12,9 @@ Plotly.d3.csv('https://raw.githubusercontent.com/TheDataMavericks/DK/master/js/c
     zmin: 0,
     zmax: 17000,
     colorscale: [
-        [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
-        [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
-        [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
+        [0, 'rgb(242,240,247)'], [2500, 'rgb(218,218,235)'],
+        [5000, 'rgb(188,189,220)'], [7500, 'rgb(158,154,200)'],
+        [10000, 'rgb(117,107,177)'], [15000, 'rgb(84,39,143)']
     ],
     colorbar: {
         title: 'Cars',
